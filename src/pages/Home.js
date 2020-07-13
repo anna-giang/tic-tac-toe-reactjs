c319a675f9c63bbd9fdaa55a76fddab9117161a6
Anna <anna.giang1999@gmail.com>
2020-07-13 15:04:26 +1000

    Rearrange Home page
    Aligned and positioned elements of the Home landing page.
    Added game description, and descriptions to the buttons.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,22 +6,33 @@ import '../index.css';
 class Home extends React.Component {
   render() {
     return (
-      <div class="container">
-        <div class="row" id="main-heading">
+      <div class="d-flex flex-column" style={{paddingTop: "120px"}}>
+        <div class="d-flex justify-content-center" id="main-heading">
           TIC TAC TOE
         </div>
-        <div class="row">
+        <div class="d-flex justify-content-center" id="main-desc">
+          <p>The first to fill up a row, column or diagonal wins!</p>
         </div>
-        <div class="row">
-          <div class="col">
+
+        <div class="d-flex justify-content-center">
+          <div class="d-flex col-4 justify-content-center">
             <Link to="/mytictactoe">
               <button type="button" class="btn btn-danger btn-lg">CUSTOM GAME</button>
             </Link>
           </div>
-          <div class="col">
+          
+          <div class="d-flex col-4 justify-content-center">
             <Link to="/tictactoe">
               <button type="button" class="btn btn-danger btn-lg">REGULAR GAME</button>
             </Link>
+          </div>
+        </div>
+        <div class="d-flex justify-content-center">
+          <div class="d-flex col-4 justify-content-center" id='inline-desc'>
+            <p>Customise your own settings!</p>
+          </div>
+          <div class="d-flex col-4 justify-content-center" id='inline-desc'>
+            <p>Play with 'X' and 'O' on regular 3x3 grid!</p>
           </div>
         </div>
       </div>
